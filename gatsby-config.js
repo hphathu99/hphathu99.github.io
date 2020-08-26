7df3141ad3f6472ee946b33bb386672de1b74825
Thu Hoang <hphathu99@Thus-MacBook-Pro.local>
2020-08-26 02:14:12 -0400

    resolve diff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,3 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /bad-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
-};
-
 module.exports = {
   pathPrefix: '/hphathu99.github.io',
   plugins: [
